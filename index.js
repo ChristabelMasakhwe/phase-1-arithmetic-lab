d1bd540448af902b2e7613b66e4c81f7eefe5eb9
Christabel <christabel.masakhwe@student.moringaschool.com>
2022-08-16 12:14:25 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
-// Write your code here
+let num1 = 2
+let num2 = 31
+let multiply = 62
+let random = 4
+Number.isInteger = true
+let max = (4,8,12,16,20)
+let num3 = 14
+let num4 = 5
+let mod = 4
